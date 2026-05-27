11826e54f819447b0f684c2caf0e4b6a5c5425bf
CATIN--RICO Charly <charly.catin-rico@etu.univ-nantes.fr>
2026-05-28 00:19:20 +0200

    ajout d'un db via sqlite3 + modif add supp et see + fonctionne toujours sur le terminal

--- main.cpp
+++ main.cpp
@@ -3,49 +3,83 @@
 //
 #include <iostream>
 #include <vector>
+#include <string>
+#include <sqlite3.h>
+
+
+// la fonction de callback pour recupere rle resultat de la db
+int callback(void* NotUsed, int col, char** val, char** azColName) {
+
+    // On boucle sur chaque colonne de la ligne en cours
+    for (int i = 0; i < col; i++) {
+        // Affiche : "NomDeLaColonne = Valeur"
+        std::cout << azColName[i] << " = " << (val[i] ? val[i] : "NULL") << " | ";
+    }
+
+    // Un saut de ligne pour séparer chaque tâche (chaque ligne de la BDD)
+    std::cout << std::endl;
+
+    // On doit toujours retourner 0 pour dire à SQLite de passer à la ligne suivante
+    return 0;
+}
+
 
 // fonction add
-void addTask(std::vector<std::string> &nl) {
+void addTask(sqlite3* db, char* &dbError) {
 
     std::string task;
     std::cout << "Enter task : " << std::endl;
     std::getline(std::cin, task);
-    nl.push_back(task);
+    std::string request;
+
+    request = "INSERT INTO Tasks (task) VALUES('" + task + "');";
+
+    sqlite3_exec(db, request.c_str(), nullptr, nullptr, &dbError);
+
+    if (dbError != nullptr) {
+        std::cerr << "Error during the process failure " << dbError << std::endl;
+    }
+
+    else {
+        std::cout << "Task added succes" << std::endl;
+    }
 
 }
 
 
 
 // fonction supp
-void delTask(std::vector<std::string> &nl) {
+void delTask(sqlite3* db, char* &dbError) {
 
-    // check size
-    if (nl.empty()) {
-        std::cout << "No tasks" << std::endl;
-        return;
+    std::string taskID;
+    std::cout << "Enter the Task ID : " << std::endl;
+    std::cin >>taskID;
+
+    std::string request;
+    const int x = std::stoi(taskID);
+
+    request = "DELETE FROM Tasks WHERE idTask = " + std::to_string(x) + ";";
+    sqlite3_exec(db, request.c_str(), nullptr, nullptr, &dbError);
+    if (dbError != nullptr) {
+        std::cerr << "Error during the process failure " << dbError << std::endl;
     }
-
-    try {
-        std::string s;
-        std::cout << "Enter index of the task you want to delete : " << std::endl;
-        std::getline(std::cin, s);
-        const int n = stoi(s);
-        nl.erase(nl.begin() + n);   // utilise le begin car il faut un iterateur de position et pas un simple nombre
-    }
-
-    catch (const std::invalid_argument& error) {
-        std::cout << "must be a number between 0 and " << nl.size() << std::endl;
+    else {
+        std::cout << "Task deleted succes" << std::endl;
     }
 
 }
 
 
 // fonction lire tout
-void seeTasks( std::vector<std::string> nl) {
-    for (size_t i = 0; i < nl.size(); i++) {
-        std::cout << i << " - " << nl[i] << std::endl;
-    }
+void seeTasks( sqlite3* db, char* &dbError) {
+    sqlite3_exec(db, "SELECT * FROM Tasks", callback, nullptr, &dbError);
 
+    if (dbError != nullptr) {
+        std::cerr << "Error during the process failure "<< dbError << std::endl;
+    }
+    else {
+        std::cout << "End of the tasks" << std::endl;
+    }
 }
 
 
@@ -54,6 +88,24 @@ int main() {
 
     std::vector<std::string> nameList = {};     // liste des taches à réaliser
     std::string userInput;
+    sqlite3* db = nullptr;                                // db for the tasks
+    char* dbError = nullptr;
+
+
+    // database creation
+    int dbInitResult = sqlite3_open("tasks.db", &db);
+
+    // check db opening
+    if (dbInitResult != SQLITE_OK ) {
+        std::cerr << "Can't open the database failure " << dbError << std::endl;    // cerr because error message
+    }
+    else {
+        std::cout << "Database opened succes" << std::endl;
+    }
+
+    sqlite3_exec(db, "CREATE TABLE Tasks (idTask INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT)", nullptr, nullptr, &dbError);
+
+
 
     // boucle principale
     while (true) {
@@ -62,17 +114,17 @@ int main() {
 
         if (userInput == "1") {
             // see tasks function
-            seeTasks(nameList);
+            seeTasks(db, dbError);
         }
 
         else if (userInput == "2") {
             // add task function
-            addTask(nameList);
+            addTask(db, dbError);
         }
 
         else if (userInput == "3") {
             // del task function
-            delTask(nameList);
+            delTask(db, dbError);
         }
 
         else if (userInput == "q") {
